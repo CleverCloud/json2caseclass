1ab1ed7575ddee3b18edd63808badba1b9457364
Quentin ADAM <quentin.adam@waxzce.org>
2013-02-20 17:51:41 +0100

    erf id

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 $(function(){
    $('#pastejsonform').submit(function(e){
       e.preventDefault();
-      $('#optionzone').html('<form id="pastejsonform">'
+      $('#optionzone').html('<form id="json_analisys_zone">'
          +'                 <fieldset>'
          +'                 <legend>Json analysis</legend>'
          +'                 <div id="classesplace"></div>'
          +'                 </fieldset>'
          +'                 </form>');
+         
+         
       
    });
 
