4179f8f78339b8a20869e0a2ca3630eda288096c
Quentin ADAM <quentin.adam@waxzce.org>
2013-02-20 17:51:00 +0100

    first construction json analysis zone

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 $(function(){
    $('#pastejsonform').submit(function(e){
-      
+      e.preventDefault();
+      $('#optionzone').html('<form id="pastejsonform">'
+         +'                 <fieldset>'
+         +'                 <legend>Json analysis</legend>'
+         +'                 <div id="classesplace"></div>'
+         +'                 </fieldset>'
+         +'                 </form>');
       
    });
 
