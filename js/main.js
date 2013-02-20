bfee11eb448f12eecc0f52f37cb59a8f48c55a72
Quentin ADAM <quentin.adam@waxzce.org>
2013-02-20 20:24:11 +0100

    collections OHW

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 $(function(){
    $('#pastejsonform').submit(function(e){
       e.preventDefault();
-      $('#optionzone').html('<form id="json_analisys_zone">'
-         +'                 <fieldset>'
-         +'                 <legend>Json analysis</legend>'
+      $('#optionzone').html('<form class="form-horizontal" id="json_analisys_zone">'
+         +'                 <h2>Json analysis</h2>'
          +'                 <div id="alertplace"></div>'
          +'                 <div id="classesplace"></div>'
-         +'                 <button type="submit" class=" pull-right btn btn-primary"><i class="icon-bolt"></i> re-generate</button>'
-         +'                 </fieldset>'
+         +'                 <button type="submit" class=" pull-right btn btn-primary"><i class="icon-cogs"></i> re-generate</button>'
          +'                 </form>');
          
       $('#json_analisys_zone').submit(re_generate_scala);
@@ -45,12 +43,22 @@ $(function(){
 });
 
 var analyse_object = function(o, oname){
-   oname = (oname.charAt(0).toUpperCase() + oname.substring(1));
-   var elem = $(t.one_class({oname:oname}));
-   var elem_u = elem.find('ul');
+   oname = generate_name(oname);
+   var sign = generate_signature(o);
+   if($('#class_'+sign).length > 0){
+      console.log('class already analyse');
+   }else{
+   var elem = $(t.one_class({oname:oname, sha: sign}));
+   var elem_u = elem.find('div.ul');
+   if(_.size(o) > 22){
+      $('#alertplace').append(t.error({value:'the '+ oname + ' class is exceding 22 fields'}));
+   }else{
+   
    
    _.each(o, function(value, key, list){
       var ts = "String";
+      var sha = "";
+      var disabled = "";
       
       
       if(_.isString(value)){
@@ -59,22 +67,41 @@ var analyse_object = function(o, oname){
       if(_.isNumber(value)){
          ts = "Long";
       }
-       
+      if(_.isBoolean(value)){
+         ts = "Boolean";
+      }
       
-      elem_u.append(t.one_line({name:key,typescala:ts}));
+      if(_.isArray(value)){
+         ts = "ARRAY";
+      }
+      
+      if(_.isObject(value) && !_.isArray(value)){
+         ts = generate_name(key);
+         disabled = "disabled";
+         sha = generate_signature(value);
+         analyse_object(value, key);
+      }
+      if(_.isDate(value)){
+         ts = "Date";
+      }
+      
+      
+      elem_u.append(t.one_line({name:key,typescala:ts,sha:sha,disabled:disabled}));
    }, this); 
+   elem.append(t.info({value:elem_u.find('.li').length+' fields'}));
    
    $('#classesplace').append(elem);
-   
+   }
+   }
 };
 
 var generate_scala = function(el){
    var content = "";
    _.each(el.find('.one_class'), function(value, key, list){
       value = $(value);
-      var props = _.map(value.find('li'), function(v, k, l){ 
+      var props = _.map(value.find('.li'), function(v, k, l){ 
          var v = $(v);
-         return v.find('span.pname').text() + ':' + v.find('input.typescala').val(); 
+         return v.find('label').text() + ':' + v.find('input.typescala').val(); 
       }, this);
       content += t.one_scala_cclass({cname:value.find('input.class_name').val(), ccontent: props.join(', ')}) + '\n';
    }, this); 
@@ -84,6 +111,14 @@ var generate_scala = function(el){
 var re_generate_scala =function(e){
       e.preventDefault();
       generate_scala($('#classesplace'));
+};
+
+var generate_signature =function(o){
+   return SHA1(_.map(_.keys(o), function(n){ return n.toLowerCase(); }).sort().join('|'));
+};
+
+var generate_name = function(oname){
+   return (oname.charAt(0).toUpperCase() + oname.substring(1));
 };
 
 
@@ -96,13 +131,22 @@ var t = {
          +'<button type="button" class="close" data-dismiss="alert">&times;</button>'
          +'<%= value %>'
          +'</div>'),
-   one_line :  _.template('<li>'
-         +'<span class="pname"><%= name %></span> : ' 
-         +'<input class="typescala" type="text" value="<%= typescala %>" />'
-         +'</li>'),         
-   one_class :  _.template('<div id="class_<%= oname %>" class="one_class">'
-         +'<input class="class_name" type="text" value="<%= oname %>" />'
-         +'<ul></ul>'
+   info :  _.template('<div class="alert alert-info">'
+         +'<button type="button" class="close" data-dismiss="alert">&times;</button>'
+         +'<%= value %>'
+         +'</div>'),
+   one_line :  _.template('<div class="li control-group">'
+         +'<label class="control-label"><%= name %></label> ' 
+         +'<div class="controls">' 
+         +'<input class="typescala" <%= disabled %> type="text" data-signature-class="<%= sha %>" value="<%= typescala %>" />'
+         +'</div>'
+         +'</div>'
+         +''),         
+   one_class :  _.template('<div id="class_<%= sha %>" class="one_class">'
+         +'<fieldset>'
+         +'<i class="icon-bookmark"></i> <input class="class_name" type="text" value="<%= oname %>" />'
+         +'<div class="ul"></div>'
+         +'</fieldset>'
          +'</div>'),        
    one_scala_cclass : _.template('case class <%= cname %>(<%= ccontent %>)'),
    one_scala_props : _.template('<%= pname %>\:<%= ptype %>')
