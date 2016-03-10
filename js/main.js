d4ebf0c9c5996d7ceca429f5c82f04ff531609a4
Quentin ADAM <quentin.adam@waxzce.org>
2016-03-10 15:31:43 +0100

    filter reserved words

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@ $(function(){
       $('#pastejsonform textarea').val('[{"coordinates":null,"favorited":false,"truncated":false,"created_at":"Mon Sep 03 13:24:14 +0000 2012","id_str":"242613977966850048","entities":{"urls":[],"hashtags":[],"user_mentions":[{"name":"Jason Costa","id_str":"14927800","id":14927800,"indices":[0,11],"screen_name":"jasoncosta"},{"name":"Matt Harris","id_str":"777925","id":777925,"indices":[12,26],"screen_name":"themattharris"},{"name":"ThinkWall","id_str":"117426578","id":117426578,"indices":[109,119],"screen_name":"thinkwall"}]},"in_reply_to_user_id_str":"14927800","contributors":null,"text":"@jasoncosta @themattharris Hey! Going to be in Frisco in October. Was hoping to have a meeting to talk about @thinkwall if you\'re around?","retweet_count":0,"in_reply_to_status_id_str":null,"id":242613977966850048,"geo":null,"retweeted":false,"in_reply_to_user_id":14927800,"place":null,"user":{"profile_sidebar_fill_color":"EEEEEE","profile_sidebar_border_color":"000000","profile_background_tile":false,"name":"Andrew Spode Miller","profile_image_url":"http://a0.twimg.com/profile_images/1227466231/spode-balloon-medium_normal.jpg","created_at":"Mon Sep 22 13:12:01 +0000 2008","location":"London via Gravesend","follow_request_sent":false,"profile_link_color":"F31B52","is_translator":false,"id_str":"16402947","entities":{"url":{"urls":[{"expanded_url":null,"url":"http://www.linkedin.com/in/spode","indices":[0,32]}]},"description":{"urls":[]}},"default_profile":false,"contributors_enabled":false,"favourites_count":16,"url":"http://www.linkedin.com/in/spode","profile_image_url_https":"https://si0.twimg.com/profile_images/1227466231/spode-balloon-medium_normal.jpg","utc_offset":0,"id":16402947,"profile_use_background_image":false,"listed_count":129,"profile_text_color":"262626","lang":"en","followers_count":2013,"protected":false,"notifications":null,"profile_background_image_url_https":"https://si0.twimg.com/profile_background_images/16420220/twitter-background-final.png","profile_background_color":"FFFFFF","verified":false,"geo_enabled":true,"time_zone":"London","description":"Co-Founder/Dev (PHP/jQuery) @justFDI. Run @thinkbikes and @thinkwall for events. Ex tech journo, helps run @uktjpr. Passion for Linux and customises everything.","default_profile_image":false,"profile_background_image_url":"http://a0.twimg.com/profile_background_images/16420220/twitter-background-final.png","statuses_count":11550,"friends_count":770,"following":null,"show_all_inline_media":true,"screen_name":"spode"},"in_reply_to_screen_name":"jasoncosta","source":"<a href=\\"http://www.journotwit.com\\" rel=\\"nofollow\\">JournoTwit</a>","in_reply_to_status_id":null},{"coordinates":{"coordinates":[121.0132101,14.5191613],"type":"Point"},"favorited":false,"truncated":false,"created_at":"Mon Sep 03 08:08:02 +0000 2012","id_str":"242534402280783873","entities":{"urls":[],"hashtags":[{"text":"twitter","indices":[49,57]}],"user_mentions":[{"name":"Jason Costa","id_str":"14927800","id":14927800,"indices":[14,25],"screen_name":"jasoncosta"}]},"in_reply_to_user_id_str":null,"contributors":null,"text":"Got the shirt @jasoncosta thanks man! Loving the #twitter bird on the shirt :-)","retweet_count":0,"in_reply_to_status_id_str":null,"id":242534402280783873,"geo":{"coordinates":[14.5191613,121.0132101],"type":"Point"},"retweeted":false,"in_reply_to_user_id":null,"place":null,"user":{"profile_sidebar_fill_color":"EFEFEF","profile_sidebar_border_color":"EEEEEE","profile_background_tile":true,"name":"Mikey","profile_image_url":"http://a0.twimg.com/profile_images/1305509670/chatMikeTwitter_normal.png","created_at":"Fri Jun 20 15:57:08 +0000 2008","location":"Singapore","follow_request_sent":false,"profile_link_color":"009999","is_translator":false,"id_str":"15181205","entities":{"url":{"urls":[{"expanded_url":null,"url":"http://about.me/michaelangelo","indices":[0,29]}]},"description":{"urls":[]}},"default_profile":false,"contributors_enabled":false,"favourites_count":11,"url":"http://about.me/michaelangelo","profile_image_url_https":"https://si0.twimg.com/profile_images/1305509670/chatMikeTwitter_normal.png","utc_offset":28800,"id":15181205,"profile_use_background_image":true,"listed_count":61,"profile_text_color":"333333","lang":"en","followers_count":577,"protected":false,"notifications":null,"profile_background_image_url_https":"https://si0.twimg.com/images/themes/theme14/bg.gif","profile_background_color":"131516","verified":false,"geo_enabled":true,"time_zone":"Hong Kong","description":"Android Applications Developer,  Studying Martial Arts, Plays MTG, Food and movie junkie","default_profile_image":false,"profile_background_image_url":"http://a0.twimg.com/images/themes/theme14/bg.gif","statuses_count":11327,"friends_count":138,"following":null,"show_all_inline_media":true,"screen_name":"mikedroid"},"in_reply_to_screen_name":null,"source":"<a href=\\"http://twitter.com/download/android\\" rel=\\"nofollow\\">Twitter for Android</a>","in_reply_to_status_id":null}]');
       $('#pastejsonform').submit();
    });
-   
+
    $("#test_button2").click(function(){
       $('#pastejsonform textarea').val('{"results":[{"address_components":[{"long_name":"1600","short_name":"1600","types":["street_number"]},{"long_name":"Amphitheatre Pkwy","short_name":"Amphitheatre Pkwy","types":["route"]},{"long_name":"Mountain View","short_name":"Mountain View","types":["locality","political"]},{"long_name":"Santa Clara","short_name":"Santa Clara","types":["administrative_area_level_2","political"]},{"long_name":"California","short_name":"CA","types":["administrative_area_level_1","political"]},{"long_name":"United States","short_name":"US","types":["country","political"]},{"long_name":"94043","short_name":"94043","types":["postal_code"]}],"formatted_address":"1600 Amphitheatre Pkwy, Mountain View, CA 94043, USA","geometry":{"location":{"lat":37.42291810,"lng":-122.08542120},"location_type":"ROOFTOP","viewport":{"northeast":{"lat":37.42426708029149,"lng":-122.0840722197085},"southwest":{"lat":37.42156911970850,"lng":-122.0867701802915}}},"types":["street_address"]}],"status":"OK"}');
       $('#pastejsonform').submit();
    });
-   
+
 });
 $(function(){
    $('#pastejsonform').submit(function(e){
@@ -23,7 +23,7 @@ $(function(){
          +'                 <div id="classesplace"></div>'
          +'                 <button type="submit" class=" pull-right btn btn-primary"><i class="icon-cogs"></i> re-generate</button>'
          +'                 </form>');
-         
+
       $('#json_analisys_zone').submit(re_generate_scala);
       var o = null;
       try{
@@ -32,39 +32,42 @@ $(function(){
          $('#alertplace').append(t.error({value:'The json root is invalid...'}));
          return 1;
       }
-      
+
       if(_.isArray(o)){
          $('#alertplace').append(t.alert({value:'The json root is an array, only the first entity will be analyse...'}));
          o = o[0];
       }
-      
+
       if(!_.isObject(o)){
          $('#alertplace').append(t.error({value:'The json root is not an object, cannot do anything for you...'}));
          return 1;
       }
-      
-      
-      
-      
+
+
+
+
       analyse_object(o, 'r00tJsonObject');
-      
-      
+
+
       $('#alertplace').append(t.info({value:$('#classesplace div.one_class').length+' case class generated'}));
-      
+
       $('input.class_name').each(function(i,ii){
          maj_name({target:ii});
       });
-      
+
       generate_scala($('#classesplace'));
-      
+
       $('input.class_name').change(maj_name);
-      
+
       $('#classesplace input').change(re_generate_scala);
-      
-      
+
+
    });
 
 });
+
+var scala_words =  ['abstract','case','catch','class','def','do','else','extends','false','final','finally','for','forSome','if','implicit','import','lazy','match','new','null','object','override','package','private','protected','return','sealed','super','this','throw','trait','try','true','type','val','var','while','with','yield'];
+var scala_chars = ['-', '_']
 
 var analyse_object = function(o, oname){
    oname = generate_name(oname);
@@ -77,15 +80,15 @@ var analyse_object = function(o, oname){
    if(_.size(o) > 22){
       $('#alertplace').append(t.error({value:'the '+ oname + ' class is exceding 22 fields, generated but it will not work, due to the Product arity limitation'}));
    }
-   
-   
+
+
    _.each(o, function(value, key, list){
       var ts = "String";
       var sha = "";
       var list = "";
       var disabled = "";
-      
-      
+
+
       if(_.isString(value)){
          ts = "String";
       }
@@ -98,9 +101,9 @@ var analyse_object = function(o, oname){
       if(_.isDate(value)){
          ts = "Date";
       }
-      
+
       if(_.isArray(value)){
-         if(is_value_consistent(value)){        
+         if(is_value_consistent(value)){
             list='List';
             disabled = "disabled";
             ts = generate_name(list + '['+generate_name(key)+']');
@@ -113,7 +116,7 @@ var analyse_object = function(o, oname){
                }else{
                   var vv = value[0];
                   var ts2 = "String"
-                  disabled = "";    
+                  disabled = "";
                   if(_.isString(vv)){
                      ts2 = "String";
                   }
@@ -133,15 +136,15 @@ var analyse_object = function(o, oname){
             $('#alertplace').append(t.error({value:'the '+ oname +' '+key+ ' field is prentending an array but not consistent'}));
          }
       }
-      
+
       if(_.isObject(value) && !_.isArray(value)){
-         /* THERE IS THE MAP CODE 
-         
+         /* THERE IS THE MAP CODE
+
          looking for a good way to choose between map and object
-         
+
           if(is_value_consistent(value)){
              list='Map';
-             
+
              for(var i in value){
                 var ts2 = "ERRORFOUND";
                 var vv = value[i];
@@ -158,7 +161,7 @@ var analyse_object = function(o, oname){
                    ts2 = "Date";
                 }
                 ts="Map[String,"+ts2+"]";
-                
+
                 if(_.isObject(vv)){
                    ts2 = "Object";
                    sha = generate_signature(vv);
@@ -179,20 +182,20 @@ var analyse_object = function(o, oname){
          analyse_object(value, key);
       //   }
       }
-     
-      
-      
+
+
+
       elem_u.append(t.one_line({name:key,typescala:ts,sha:sha,disabled:disabled,list:list, oname:oname}));
-   }, this); 
+   }, this);
    elem.append(t.info({value:elem_u.find('.li').length+' fields'}));
-   
+
    $('#classesplace').append(elem);
    }
-   
+
 };
 
 var sanitize_var_name = function(name){
-   if(name.match(/[_a-zA-Z0-9]+/) == name){
+   if(name.match(/[_a-zA-Z0-9]+/) == name && !_.contains(scala_words, name)){
       return name;
    }else{
       return '`' + name + '`';
@@ -203,16 +206,16 @@ var generate_scala = function(el){
    var content = "";
    _.each(el.find('.one_class'), function(value, key, list){
       value = $(value);
-      var props = _.map(value.find('.li'), function(v, k, l){ 
+      var props = _.map(value.find('.li'), function(v, k, l){
          var v = $(v);
          var sst = v.find('input.typescala').val();
           if ( v.find('input.optional_value[type="checkbox"]').prop("checked") ){
              sst = 'Option['+sst+']';
-          } 
+          }
          return '  ' + sanitize_var_name(v.find('label.keyname').text()) + ': ' + sst;
       }, this);
       content += t.one_scala_cclass({cname:value.find('input.class_name').val(), ccontent: props.join(',\n')}) + '\n';
-   }, this); 
+   }, this);
    $('#caseclassform textarea').val(content);
    $('#mycodeis').html(t.scala_code({code:content}));
    sh_highlightDocument();
@@ -243,7 +246,7 @@ var is_value_consistent = function(o){
    }else{
    if(!_.isArray(o)){
       o = _.values(o);
-   } 
+   }
    var n = o[0];
    var nn = (_.isObject(n) ? generate_signature(n) : typeof n);
    return _.every(o, function(n){ return (_.isObject(n) ? generate_signature(n) : typeof n) == nn; }, this);
@@ -255,9 +258,9 @@ var generate_signature_collection =function(o){
    }else{
    if(!_.isArray(o)){
       o = _.values(o);
-   } 
+   }
    return generate_signature(o[0]);
-}  
+}
 };
 var generate_signature =function(o){
    if(_.isObject(o)){
@@ -286,20 +289,20 @@ var t = {
          +'<%= value %>'
          +'</div>'),
    one_line :  _.template('<div class="li control-group">'
-         +'<label class="keyname control-label"><%= name %></label> ' 
-         +'<div class="controls">' 
+         +'<label class="keyname control-label"><%= name %></label> '
+         +'<div class="controls">'
          +'<div class="input-append"><input class="typescala" <%= disabled %> type="text" data-signature-class="<%= sha %>" data-list="<%= list %>" value="<%= typescala %>" />'
          +' <span class="add-on"><input class="optional_value" type="checkbox" value="" id="chkb_<%= oname %>_<%= name %>" /><label class="label_chkbr" for="chkb_<%= oname %>_<%= name %>"> optional</label></span>'
          +'</div>'
          +'</div>'
          +'</div>'
-         +''),         
+         +''),
    one_class :  _.template('<div id="class_<%= sha %>" class="one_class">'
          +'<fieldset>'
          +'<div class="class_title"><i class="icon-leaf"></i> <input class="class_name" data-signature-class="<%= sha %>" type="text" value="<%= oname %>" /></div>'
          +'<div class="ul"></div>'
          +'</fieldset>'
-         +'</div>'),        
+         +'</div>'),
    one_scala_cclass : _.template('case class <%= cname %>(\n<%= ccontent %>\n)'),
    one_scala_props : _.template('<%= pname %>\:<%= ptype %>'),
    scala_code : _.template('<pre class="sh_scala"><%= code %></pre>')
